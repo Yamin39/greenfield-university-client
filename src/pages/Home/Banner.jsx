bb7232c27db551e34b5602773ecf8817df7c8b1b
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-01-07 01:08:24 +0600

    I am going to create banner section

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,9 @@
 const Banner = () => {
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="bg-gray-100 h-[400px] rounded-xl mt-28 mb-10 flex items-center justify-center">
+      <div className="bg-gray-100 h-[400px] rounded-xl mt-28 mb-10 flex items-center justify-center flex-col">
         <h1 className="text-3xl font-medium">Banner</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente distinctio voluptas assumenda provident dolorem. Facilis, porro? Veritatis repellat dignissimos sequi reiciendis incidunt! Reiciendis, magnam est. Laboriosam earum exercitationem quam debitis!</p>
       </div>
     </div>
   );
