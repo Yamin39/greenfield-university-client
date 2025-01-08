2e7f1a2349f74b26b5c6b0f92ba54a796a30f079
MukterHossain <mukter3075@gmail.com>
2025-01-08 22:50:07 +0600

    added campusLife component in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Banner from "./Banner";
+import CampusLife from "./CampusLife";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <About></About>
+      <CampusLife></CampusLife>
     </div>
   );
 };
