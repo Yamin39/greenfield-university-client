2b6e978a170500b3bc96ae72d34c840cdc57ea35
mehedifiz <mehedi.shanto.contract@gmail.com>
2025-01-09 16:59:03 +0600

    df

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -23,36 +23,109 @@ const About = () => {
           <div className="mt-12">
             <Tabs>
               <TabList>
-                <Tab> <span className={'hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative'}>About EduBlink</span></Tab>
-                <Tab><span className={'hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative'}>Our Mission</span></Tab>
-                <Tab><span className={'hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative'}>Our Vision</span></Tab>
+                <Tab>
+                  {" "}
+                  <span
+                    className={
+                      "hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative"
+                    }
+                  >
+                    About EduBlink
+                  </span>
+                </Tab>
+                <Tab>
+                  <span
+                    className={
+                      "hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative"
+                    }
+                  >
+                    Our Mission
+                  </span>
+                </Tab>
+                <Tab>
+                  <span
+                    className={
+                      "hover:text-red-500 text-xl font-semibold duration-300 after:w-0 hover:after:w-full after:duration-300 after:h-0.5 after:absolute after:-bottom-1 after:bg-red-500 after:left-0 relative"
+                    }
+                  >
+                    Our Vision
+                  </span>
+                </Tab>
               </TabList>
 
               <TabPanel>
                 <div>
-                    <p className="text-[#888888] text-lg my-6">Magna aliquaenim minim veniam quis nostrud exercitation ullamco laborisLorem ipsum dolor sit amet consectetur adipisicing elit sed do eius tempor incididunt labore.</p>
-                    <div className="mt-4 space-y-3">
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Education award achived</span> </p>
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark  size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Available online courses</span> </p>
-                    </div>
+                  <p className="text-[#888888] text-lg my-6">
+                    Magna aliquaenim minim veniam quis nostrud exercitation
+                    ullamco laborisLorem ipsum dolor sit amet consectetur
+                    adipisicing elit sed do eius tempor incididunt labore.
+                  </p>
+                  <div className="mt-4 space-y-3">
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Education award achived</span>{" "}
+                    </p>
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Available online courses</span>{" "}
+                    </p>
+                  </div>
                 </div>
               </TabPanel>
               <TabPanel>
                 <div>
-                    <p className="text-[#888888] text-lg my-6">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.</p>
-                    <div className="mt-4 space-y-3">
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Industry Expert Instructor</span> </p>
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark  size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Up-to-Date Course Content</span> </p>
-                    </div>
+                  <p className="text-[#888888] text-lg my-6">
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa.
+                  </p>
+                  <div className="mt-4 space-y-3">
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Industry Expert Instructor</span>{" "}
+                    </p>
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Up-to-Date Course Content</span>{" "}
+                    </p>
+                  </div>
                 </div>
               </TabPanel>
               <TabPanel>
                 <div>
-                    <p className="text-[#888888] text-lg my-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer.</p>
-                    <div className="mt-4 space-y-3">
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Online Remote Learning</span> </p>
-                        <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]"><GiCheckMark  size={25} className="text-[#EE4A62] "></GiCheckMark> <span>Lifetime Access For Learning</span> </p>
-                    </div>
+                  <p className="text-[#888888] text-lg my-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exer.
+                  </p>
+                  <div className="mt-4 space-y-3">
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Online Remote Learning</span>{" "}
+                    </p>
+                    <p className="text-lg flex flex-row gap-x-5 items-center font-semibold text-[#181818]">
+                      <GiCheckMark
+                        size={25}
+                        className="text-[#EE4A62] "
+                      ></GiCheckMark>{" "}
+                      <span>Lifetime Access For Learning</span>{" "}
+                    </p>
+                  </div>
                 </div>
               </TabPanel>
             </Tabs>
@@ -60,10 +133,18 @@ const About = () => {
         </div>
         {/* Right side */}
         <div className="">
-            <div className="relative flex flex-col  lg:items-center">
-                <img className="rounded-xl w-[390px] h-[390px]" src="https://i.ibb.co.com/prPywNG/about-1.webp" alt="pic" />
-                <img className="absolute hidden md:block  md:translate-x-4 transition  rounded-xl md:right-64 md:-bottom-16 lg:-right-3 lg:-bottom-16" src="https://i.ibb.co.com/RvM0dkK/about-2.webp" alt="pic" />
-            </div>
+          <div className="relative flex flex-col  lg:items-center">
+            <img
+              className="rounded-xl w-[390px] h-[390px]"
+              src="https://i.ibb.co.com/prPywNG/about-1.webp"
+              alt="pic"
+            />
+            <img
+              className="absolute hidden md:block  md:translate-x-4 transition  rounded-xl md:right-64 md:-bottom-16 lg:-right-3 lg:-bottom-16"
+              src="https://i.ibb.co.com/RvM0dkK/about-2.webp"
+              alt="pic"
+            />
+          </div>
         </div>
       </div>
       {/* Card */}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <About></About>
 
   
-<
+
       <Testimonials/>
     </div>
   );
