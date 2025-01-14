881a71bc2bd405a835e1218b4ce7e89d83acb030
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-01-14 21:25:27 +0600

    i have mounte the about title to the shared folder

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,15 +1,15 @@
 import SharedBanner from "../../shared/SharedBanner";
-import AboutOverview from "./AboutOverview";
 import AboutUniversity from "./AboutUniversity";
 import Apply from "./Apply";
 import OfferOverview from "./OfferOverview";
 import TuitionFees from "./TuitionFees";
+import SharedAbout from "../../shared/SharedAbout";
 
 const Overview = () => {
    return (
       <div>
          <SharedBanner title="Overview" />
-         <AboutOverview />
+         <SharedAbout title="At Stanford, we practice holistic admission. This means that each piece in your application is reviewed as part of an integrated and comprehensive whole."/>
          <OfferOverview />
          <AboutUniversity />
          <TuitionFees />
