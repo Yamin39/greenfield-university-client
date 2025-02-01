20eadd1056a43a1cba85b72f6bb6a159abf72285
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-02-01 12:41:17 +0600

    added My Profile for students

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -50,6 +50,7 @@ import ManageTestimonials from "../pages/Testimonials/ManageTestimonials";
 import PrivacyPolicy from "../pages/privacyPolicy/PrivacyPolicy";
 import AddCourse from "../Dashboard/instructor/AddCourse";
 import InstructorsProfile from "../Dashboard/instructor/InstructorsProfile";
+import StudentProfile from "../Dashboard/student/StudentProfile";
 
 const router = createBrowserRouter([
   {
@@ -278,6 +279,10 @@ const router = createBrowserRouter([
       {
         path: "instructor/my-profile",
         element: <InstructorsProfile />,
+      },
+      {
+        path: "student/my-profile",
+        element: <StudentProfile />,
       }
     ]
   }
