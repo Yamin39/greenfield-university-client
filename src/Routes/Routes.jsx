6e84648f0ed42c67c1ad43a86ed64cb07fc458bd
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2025-02-13 21:22:06 +0600

    updated api url

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,79 +1,79 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Statistics from "../Dashboard/Statistics";
+import AddBlog from "../Dashboard/admin/AddBlog";
+import AddEvents from "../Dashboard/admin/AddEvents";
+import AddFaq from "../Dashboard/admin/AddFaq";
+import AddProduct from "../Dashboard/admin/AddProduct";
+import AdminProfile from "../Dashboard/admin/AdminProfile";
+import ApproveCourse from "../Dashboard/admin/ApproveCourses";
+import ContactRequests from "../Dashboard/admin/ContactRequests";
+import ManageAnnouncements from "../Dashboard/admin/ManageAnnouncements";
+import ManageBlogs from "../Dashboard/admin/ManageBlogs";
+import ManageEvents from "../Dashboard/admin/ManageEvents";
+import ManageFaq from "../Dashboard/admin/ManageFaq";
+import ManageGallery from "../Dashboard/admin/ManageGallery";
+import ManageProducts from "../Dashboard/admin/ManageProducts";
+import NewsletterRequests from "../Dashboard/admin/NewsletterRequests";
+import PostAnnouncements from "../Dashboard/admin/PostAnnouncements";
+import RegisteredInstructors from "../Dashboard/admin/RegisteredInstructors";
+import RegisteredStudents from "../Dashboard/admin/RegisteredStudents";
+import UpdateAnnouncement from "../Dashboard/admin/UpdateAnnouncement";
+import UpdateBlog from "../Dashboard/admin/UpdateBlog";
+import UpdateEvent from "../Dashboard/admin/UpdateEvent";
+import UpdateFaq from "../Dashboard/admin/UpdateFaq";
+import UpdateProduct from "../Dashboard/admin/UpdateProduct";
+import ApproveBlogs from "../Dashboard/admin/approveBlogs";
+import AddCourse from "../Dashboard/instructor/AddCourse";
+import InstructorsProfile from "../Dashboard/instructor/InstructorsProfile";
+import ManageCourse from "../Dashboard/instructor/ManageCourse";
+import ManageQuery from "../Dashboard/student/ManageQuery";
+import PurchasedCourses from "../Dashboard/student/PurchasedCourses";
+import StudentProfile from "../Dashboard/student/StudentProfile";
 import Root from "../layouts/Root";
-import Home from "../pages/Home/Home";
-import OurCourse from "../pages/course/OurCourse";
-import CourseDetails from "../pages/course/CourseDetails";
-import Overview from "../pages/overview/Overview";
 import HowtoApply from "../pages/Apply/HowtoApply";
-import TuitionFee from "../pages/tuitionFee/TuitionFee";
-import FinancialAid from "../pages/financialAid/FinancialAid";
-import Contact from "../pages/contact/Contact";
-import Tour from "../pages/Tour/Tour";
-import DateDeadline from "../pages/dateDeadline/DateDeadline";
-import Login from "../pages/Login/Login";
-import Registration from "../pages/Registration/Registration";
-import About from "../pages/about/About";
-import Faq from "../pages/Faq/Faq";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Events from "../pages/Events/Events";
+import Eventspage from "../pages/Eventspage/Eventspage";
+import Faq from "../pages/Faq/Faq";
+import Gallery from "../pages/Gallery/Gallery";
+import Home from "../pages/Home/Home";
+import InstructorDetails from "../pages/Instructor/InstructorDetails";
+import Instructors from "../pages/Instructor/Instructors";
+import UnregisteredInstructors from "../pages/Instructor/unregisteredInstructors";
+import UnregisteredStudents from "../pages/Instructor/unregisteredStudents";
+import Login from "../pages/Login/Login";
+import Payment from "../pages/Payment/Payment";
+import Policy from "../pages/Policy/Policy";
+import Queries from "../pages/Queries/Queries";
+import QueryDetails from "../pages/Queries/QueryDetails/QueryDetails";
+import Registration from "../pages/Registration/Registration";
+import ManageTestimonials from "../pages/Testimonials/ManageTestimonials";
+import Testimonials from "../pages/Testimonials/Testimonials";
+import Tour from "../pages/Tour/Tour";
+import About from "../pages/about/About";
+import AnnouncementDetails from "../pages/announcements/AnnouncementDetails";
 import Announcements from "../pages/announcements/Announcements";
 import Blogs from "../pages/blog/Blogs";
 import BlogDetails from "../pages/blog/blogDetails/BlogDetails";
-import Instructors from "../pages/Instructor/Instructors";
-import Shop from "../pages/shop/Shop";
-import Gallery from "../pages/Gallery/Gallery";
-import Dashboard from "../Dashboard/Dashboard";
-import Statistics from "../Dashboard/Statistics";
-import ManageGallery from "../Dashboard/admin/ManageGallery";
-import ShopDetails from "../pages/shop/shopDetails/ShopDetails";
-import AnnouncementDetails from "../pages/announcements/AnnouncementDetails";
-import InstructorDetails from "../pages/Instructor/InstructorDetails";
-import Policy from "../pages/Policy/Policy";
-import ShopCart from "../pages/shop/shopCart/ShopCart";
-import Wishlist from "../pages/shop/wishlist/Wishlist";
-import ManageAnnouncements from "../Dashboard/admin/ManageAnnouncements";
-import PostAnnouncements from "../Dashboard/admin/PostAnnouncements";
-import AddProduct from "../Dashboard/admin/AddProduct";
-import Eventspage from "../pages/Eventspage/Eventspage";
-import Events from "../pages/Events/Events";
-import Testimonials from "../pages/Testimonials/Testimonials";
 import Chatbot from "../pages/chatbot/Chatbot";
-import ManageBlogs from "../Dashboard/admin/ManageBlogs";
-import AddBlog from "../Dashboard/admin/AddBlog";
-import PurcheseGuide from "../pages/purcheseGuide/PurcheseGuide";
-import TermsCondition from "../pages/termsCondition/TermsCondition";
-import UpdateBlog from "../Dashboard/admin/UpdateBlog";
-import AddFaq from "../Dashboard/admin/AddFaq";
-import ManageFaq from "../Dashboard/admin/ManageFaq";
-import UpdateFaq from "../Dashboard/admin/UpdateFaq";
-import UpdateAnnouncement from "../Dashboard/admin/UpdateAnnouncement";
-import ManageTestimonials from "../pages/Testimonials/ManageTestimonials";
+import Contact from "../pages/contact/Contact";
+import CourseDetails from "../pages/course/CourseDetails";
+import OurCourse from "../pages/course/OurCourse";
+import DateDeadline from "../pages/dateDeadline/DateDeadline";
+import FinancialAid from "../pages/financialAid/FinancialAid";
+import Overview from "../pages/overview/Overview";
 import PrivacyPolicy from "../pages/privacyPolicy/PrivacyPolicy";
-import AddCourse from "../Dashboard/instructor/AddCourse";
-import InstructorsProfile from "../Dashboard/instructor/InstructorsProfile";
-import StudentProfile from "../Dashboard/student/StudentProfile";
-import ManageProducts from "../Dashboard/admin/ManageProducts";
-import UpdateProduct from "../Dashboard/admin/UpdateProduct";
-import ManageCourse from "../Dashboard/instructor/ManageCourse";
-import ContactRequests from "../Dashboard/admin/ContactRequests";
-import Queries from "../pages/Queries/Queries";
-import QueryDetails from "../pages/Queries/QueryDetails/QueryDetails";
-import Payment from "../pages/Payment/Payment";
+import PurcheseGuide from "../pages/purcheseGuide/PurcheseGuide";
+import Shop from "../pages/shop/Shop";
 import PurchasedBooks from "../pages/shop/purchased/PurchasedBooks";
 import PurchasedHistory from "../pages/shop/purchased/PurchasedHistory";
-import ApproveBlogs from "../Dashboard/admin/approveBlogs";
-import AdminProfile from "../Dashboard/admin/AdminProfile";
-import ApproveCourse from "../Dashboard/admin/ApproveCourses";
-import AddEvents from "../Dashboard/admin/AddEvents";
-import ManageEvents from "../Dashboard/admin/ManageEvents";
+import ShopCart from "../pages/shop/shopCart/ShopCart";
+import ShopDetails from "../pages/shop/shopDetails/ShopDetails";
+import Wishlist from "../pages/shop/wishlist/Wishlist";
+import TermsCondition from "../pages/termsCondition/TermsCondition";
+import TuitionFee from "../pages/tuitionFee/TuitionFee";
 import PrivateRoute from "./PrivateRoute";
-import ManageQuery from "../Dashboard/student/ManageQuery";
-import RegisteredStudents from "../Dashboard/admin/RegisteredStudents";
-import RegisteredInstructors from "../Dashboard/admin/RegisteredInstructors";
-import UnregisteredStudents from "../pages/Instructor/unregisteredStudents";
-import UnregisteredInstructors from "../pages/Instructor/unregisteredInstructors";
-import NewsletterRequests from "../Dashboard/admin/NewsletterRequests";
-import UpdateEvent from "../Dashboard/admin/UpdateEvent";
-import PurchasedCourses from "../Dashboard/student/PurchasedCourses";
 
 const router = createBrowserRouter([
   {
@@ -190,8 +190,7 @@ const router = createBrowserRouter([
       {
         path: "/announcement/:id",
         element: <AnnouncementDetails />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/announcement/${params.id}`),
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/announcement/${params.id}`),
       },
       {
         path: "/instructors",
@@ -200,8 +199,7 @@ const router = createBrowserRouter([
       {
         path: "/instructor/:id",
         element: <InstructorDetails />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/instructor/${params.id}`),
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/instructor/${params.id}`),
       },
       {
         path: "/gallery",
@@ -233,19 +231,24 @@ const router = createBrowserRouter([
       {
         path: "/queries",
         element: <Queries />,
-      }, {
-        path: 'payment',
-        element: <Payment />
+      },
+      {
+        path: "payment",
+        element: <Payment />,
       },
       {
         path: "/query-details/:id",
         element: <QueryDetails />,
-      }
+      },
     ],
   },
   {
     path: "/dashboard",
-    element: <PrivateRoute><Dashboard /></PrivateRoute>,
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
     children: [
       {
         index: true,
@@ -280,25 +283,25 @@ const router = createBrowserRouter([
         element: <AddProduct />,
       },
       {
-        path: 'manageProducts',
-        element: <ManageProducts></ManageProducts>
+        path: "manageProducts",
+        element: <ManageProducts></ManageProducts>,
       },
       {
-        path: 'updateProduct/:id',
+        path: "updateProduct/:id",
         element: <UpdateProduct></UpdateProduct>,
-        loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`)
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/product/${params.id}`),
       },
       {
-        path: 'manageProducts',
-        element: <ManageProducts></ManageProducts>
+        path: "manageProducts",
+        element: <ManageProducts></ManageProducts>,
       },
       {
-        path: 'updateProduct/:id',
+        path: "updateProduct/:id",
         element: <UpdateProduct></UpdateProduct>,
-        loader: ({ params }) => fetch(`http://localhost:5000/product/${params.id}`)
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/product/${params.id}`),
       },
       {
-        path: 'manageBlogs',
+        path: "manageBlogs",
         element: <ManageBlogs />,
       },
       {
@@ -308,8 +311,7 @@ const router = createBrowserRouter([
       {
         path: "updateBlog/:id",
         element: <UpdateBlog />,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/blogs/${params.id}`),
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/blogs/${params.id}`),
       },
       {
         path: "addFaq",
@@ -326,21 +328,20 @@ const router = createBrowserRouter([
       {
         path: "updateFaq/:id",
         element: <UpdateFaq></UpdateFaq>,
-        loader: ({ params }) =>
-          fetch(`http://localhost:5000/faqs/${params.id}`),
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/faqs/${params.id}`),
       },
       {
         path: "updateAnnouncement/:id",
         element: <UpdateAnnouncement />,
-        loader: ({ params }) => fetch(`http://localhost:5000/announcement/${params.id}`),
+        loader: ({ params }) => fetch(`https://greenfield-university-server.vercel.app/announcement/${params.id}`),
       },
       {
-        path: 'addCourse',
-        element: <AddCourse />
+        path: "addCourse",
+        element: <AddCourse />,
       },
       {
-        path: 'manageCourse',
-        element: <ManageCourse />
+        path: "manageCourse",
+        element: <ManageCourse />,
       },
       {
         path: "instructor/my-profile",
@@ -351,69 +352,68 @@ const router = createBrowserRouter([
         element: <StudentProfile />,
       },
       {
-        path: 'contactRequests',
-        element: <ContactRequests />
+        path: "contactRequests",
+        element: <ContactRequests />,
       },
       {
         path: "admin/my-profile",
-        element: <AdminProfile />
+        element: <AdminProfile />,
       },
       {
-        path: 'contactRequests',
-        element: <ContactRequests />
+        path: "contactRequests",
+        element: <ContactRequests />,
       },
       {
         path: "purchasedBooks",
         element: <PurchasedBooks />,
       },
       {
-        path: 'purchasedHistory',
-        element: <PurchasedHistory />
+        path: "purchasedHistory",
+        element: <PurchasedHistory />,
       },
       {
-        path: 'approveBlogs',
-        element: <ApproveBlogs />
+        path: "approveBlogs",
+        element: <ApproveBlogs />,
       },
       {
-        path: 'approveCourses',
-        element: <ApproveCourse />
+        path: "approveCourses",
+        element: <ApproveCourse />,
       },
       {
-        path: 'addEvents',
-        element: <AddEvents />
+        path: "addEvents",
+        element: <AddEvents />,
       },
       {
-        path: 'manageEvents',
-        element: <ManageEvents />
+        path: "manageEvents",
+        element: <ManageEvents />,
       },
       {
-        path: 'manageEvents/edit/:id',
-        element: <UpdateEvent />
+        path: "manageEvents/edit/:id",
+        element: <UpdateEvent />,
       },
       {
-        path: 'manageQuery',
-        element: <ManageQuery />
+        path: "manageQuery",
+        element: <ManageQuery />,
       },
-       
-      {
-        path: 'unregisteredStudents',
-        element: <UnregisteredStudents />
-      },
-      {
-        path: 'unregisteredInstructors',
-        element: <UnregisteredInstructors />
-      },
-      {
-        path: 'newsletterRequests',
-        element: <NewsletterRequests />
-      },
-      {
-        path: 'registeredCourse',
-        element: <PurchasedCourses />
-      },
-    ]
-  }
 
+      {
+        path: "unregisteredStudents",
+        element: <UnregisteredStudents />,
+      },
+      {
+        path: "unregisteredInstructors",
+        element: <UnregisteredInstructors />,
+      },
+      {
+        path: "newsletterRequests",
+        element: <NewsletterRequests />,
+      },
+      {
+        path: "registeredCourse",
+        element: <PurchasedCourses />,
+      },
+    ],
+  },
 ]);
 
 export default router;
